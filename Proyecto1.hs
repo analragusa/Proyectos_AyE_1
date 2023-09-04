efb92ea41e85f464deffa15f877d82e2e8f16baa
anal <144064536+analragusa@users.noreply.github.com>
2023-09-04 17:45:11 -0300

    Update Proyecto1.hs

--- Proyecto1.hs
+++ Proyecto1.hs
@@ -6,3 +6,16 @@ primIgualesA n (x:xs) | n == x = x:primIguales n xs
                       | n /= x = primIgualesA n []
 
 --10.b) Program ́a nuevamente la funci ́on utilizando takeWhile.
+primIgualesA' :: a -> [a] -> [a]
+primIgualesA' n xs = takeWhile (n == x)
+
+--11. La funci ́on primIguales toma una lista y devuelve el mayor tramo inicial de la lista cuyos elementos son todos iguales entre s ́ı.
+--11.a)
+primIguales :: [a] -> [a]
+primIguales [] = []
+primIguales (x:xs) = | x == (xs!!0) = x:primIguales xs
+                     | x /= (xs!!0) = primIguales []
+
+--11.b) Us ́a cualquier versi ́on de primIgualesA para programar primIguales. Est ́a permitido dividir en casos, pero no usar recursi ́on.
+primIguales' :: [a] -> [a]
+primIguales' xs = primIgualesA (xs!!0) xs
